061fe033057a0bf4a410c32d460f2f7f225f278f
Imalzy <imalzaya19@gmail.com>
2020-09-13 16:47:34 +0700

    Post using raw using Buffer

--- server.js
+++ server.js
@@ -7,15 +7,26 @@ const todo = [
 ];
 
 const server = http.createServer((req, res) => {
-  res.writeHead(400, {
+  res.writeHead(200, {
     "Content-Type": "application/json",
     "X-Powered-By": "DevNodeJs",
   });
+
+  let body = [];
+
+  req
+    .on("data", (chunk) => {
+      body.push(chunk);
+    })
+    .on("end", () => {
+      body = Buffer.concat(body).toString();
+      console.log(body);
+    });
+
   res.end(
     JSON.stringify({
-      success: false,
-      error: "Please add email",
-      data: null,
+      success: true,
+      data: todo,
     })
   );
 });
